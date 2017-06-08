945852627edb0e11b900f064f4d56afba29de257
TPhong <tpalwayscreative@gmail.com>
2017-06-08 18:13:00 +0700

    add

---
+++ app/src/main/java/itc_android/com/itc_android/MainActivity.java
@@ -0,0 +1,15 @@
+package itc_android.com.itc_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
